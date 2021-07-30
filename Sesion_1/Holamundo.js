d8d8bd970d531e937f0ed3cca6bd1c1d4dff770a
Jorvict Piña <jorvict15@gmail.com>
2021-07-29 21:55:34 -0500

    Se finaliza sesión 1

--- Sesion_1/Holamundo.js
+++ Sesion_1/Holamundo.js
@@ -1,1 +1,18 @@
-a
+console.log("Hola mundo");
+// alert("Hola mundo");
+
+
+// Declarar variable
+var nombre;
+
+//Asignar dato a variable
+nombre = "Jorvict";
+
+var edad = 22;
+
+var precio = 50.5;
+
+canal = "CodigoFacilito"; //Mala practica declarar variable de esta forma
+
+console.log(canal)
+document.querySelector("body").innerHTML = "Hola " + nombre;
