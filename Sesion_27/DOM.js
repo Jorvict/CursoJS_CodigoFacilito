03f0b47d53017c560ba3112d10c593a6d6ab1c45
Jorvict Piña <jorvict15@gmail.com>
2021-08-28 19:30:43 -0500

    Se finaliza sesion 27

--- Sesion_27/DOM.js
+++ Sesion_27/DOM.js
@@ -3,6 +3,8 @@ function $(selector) {
     return document.querySelector(selector);
 }
 
+bind_close();
+
 $("#btn").addEventListener("click", function () {
     var input = document.createElement("input");
 
@@ -19,6 +21,37 @@ $("#btn").addEventListener("click", function () {
 
     $("#form").appendChild(input); // Agarra un nodo y lo coloca como hijo al final
     // console.log(input)
+
+
+    my_alert("Se agregó un nuevo campo");
 })
 
+function my_alert(msg) {
+    // console.log($("body").children[1]);
+    var div = document.createElement("div");
+    div.setAttribute("class", "alert"); // Tambien se puede usar Classlist
+    div.innerHTML = msg;
+
+    var close = document.createElement("span");
+    close.setAttribute("class", "close");
+    close.style.float = "right";
+    close.innerHTML = "X";
+    div.appendChild(close);
+
+
+    $("body").insertBefore(div, $("body").firstChild);
+
+    bind_close();
+}
+
+function bind_close(params) {
+    var elements = document.querySelectorAll(".close")
+    for (var i = elements.length -1; i >= 0; i--){
+        var el = elements[i];
+        // console.log(el);
+        el.addEventListener("click", function () {
+            this.parentNode.style.display = "none";
+        })
+    }
+}
 
